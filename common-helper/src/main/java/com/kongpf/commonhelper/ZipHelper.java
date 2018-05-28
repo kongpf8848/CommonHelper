3b2ff1e7ee93e14ae1dbf02428c3e1abf4a2e65b
kongpf8848 <kongpf8848@gmail.com>
2018-05-28 22:16:38 +0800

    add ZipHelper

---
+++ common-helper/src/main/java/com/kongpf/commonhelper/ZipHelper.java
@@ -0,0 +1,55 @@
+package com.kongpf.commonhelper;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class ZipHelper {
+
+    public static void unzip(File zip, File dest) throws IOException {
+
+        ZipFile zipFile = new ZipFile(zip);
+
+        try {
+            Enumeration<ZipEntry> entries = (Enumeration<ZipEntry>) zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+
+                if (entry.isDirectory()) {
+                    (new File(dest, entry.getName())).mkdirs();
+                    continue;
+                }
+
+                OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(new File(dest, entry.getName())));
+                try {
+                    copyInputStream(zipFile.getInputStream(entry), outputStream);
+                } finally {
+                    outputStream.close();
+                }
+            }
+        } finally {
+            zipFile.close();
+        }
+    }
+
+    private static void copyInputStream(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len;
+
+        while ((len = in.read(buffer)) >= 0) {
+            out.write(buffer, 0, len);
+        }
+        out.flush();
+
+        in.close();
+        out.close();
+    }
+
+}
