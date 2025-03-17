4e51d60a99fadc52bb139fd7d3543c69620fd319
kongpf8848 <kongpf8848@gmail.com>
2025-03-17 16:17:54 +0800

    update gradle version

--- app/src/main/java/io/github/kongpf8848/commonhelper/demo/MainActivity.java
+++ app/src/main/java/io/github/kongpf8848/commonhelper/demo/MainActivity.java
@@ -15,8 +15,6 @@ import io.github.kongpf8848.commonhelper.ScreenHelper;
 import io.github.kongpf8848.commonhelper.ShortCutHelper;
 import io.github.kongpf8848.commonhelper.ToastHelper;
 
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +22,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        int[] buttonIds = {
+                R.id.button1, R.id.button2, R.id.button3,
+                R.id.button4, R.id.button5, R.id.button6,
+                R.id.button7, R.id.button8, R.id.button9,
+                R.id.button10, R.id.button11, R.id.button12,
+                R.id.button13, R.id.button14, R.id.button15
+        };
+        for (int id : buttonIds) {
+            findViewById(id).setOnClickListener(v -> {
+                switch (id) {
+                    case R.id.button1:
+                        onButton1();
+                        break;
+                    case R.id.button2:
+                        onButton2();
+                        break;
+                    case R.id.button3:
+                        onButton3();
+                        break;
+                    case R.id.button4:
+                        onButton4();
+                        break;
+                    case R.id.button5:
+                        onButton5();
+                        break;
+                    case R.id.button6:
+                        onButton6();
+                        break;
+                    case R.id.button7:
+                        onButton7();
+                        break;
+                    case R.id.button8:
+                        onButton8();
+                        break;
+                    case R.id.button9:
+                        onButton9();
+                        break;
+                    case R.id.button10:
+                        onButton10();
+                        break;
+                    case R.id.button11:
+                        onButton11();
+                        break;
+                    case R.id.button12:
+                        onButton12();
+                        break;
+                    case R.id.button13:
+                        onButton13();
+                        break;
+                    case R.id.button14:
+                        onButton14();
+                        break;
+                    case R.id.button15:
+                        onButton15();
+                        break;
+                }
+            });
+        }
+
+
     }
 
     @Override
@@ -32,17 +89,14 @@ public class MainActivity extends AppCompatActivity {
         super.onNewIntent(intent);
     }
 
-    @OnClick(R.id.button1)
     public void onButton1() {
         ToastHelper.toast("welcome to android");
     }
 
-    @OnClick(R.id.button2)
     public void onButton2() {
         ToastHelper.toast(AlgorithmHelper.getMD5("123"));
     }
 
-    @OnClick(R.id.button3)
     public void onButton3() {
 
         String packageName = "com.tencent.mm";
@@ -53,54 +107,45 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @OnClick(R.id.button4)
     public void onButton4() {
-        int width=ScreenHelper.getScreenWidth(this);
-        int height=ScreenHelper.getScreenHeight(this);
-        float density=ScreenHelper.getScreenDensity(this);
-        int statusbarHeight=ScreenHelper.getStatusbarHeight(this);
-        int navigationBarHeight=ScreenHelper.getNavigationBarHeight(this);
-        int actionBarHeight=ScreenHelper.getActionBarHeight(this);
-        Log.d("MainActivity", "width ="+width+",height="+height+",density="+density+",statusbarheight="+statusbarHeight+",navigationBarHeight="+navigationBarHeight+",actionBarHeight="+actionBarHeight);
+        int width = ScreenHelper.getScreenWidth(this);
+        int height = ScreenHelper.getScreenHeight(this);
+        float density = ScreenHelper.getScreenDensity(this);
+        int statusbarHeight = ScreenHelper.getStatusbarHeight(this);
+        int navigationBarHeight = ScreenHelper.getNavigationBarHeight(this);
+        int actionBarHeight = ScreenHelper.getActionBarHeight(this);
+        Log.d("MainActivity", "width =" + width + ",height=" + height + ",density=" + density + ",statusbarheight=" + statusbarHeight + ",navigationBarHeight=" + navigationBarHeight + ",actionBarHeight=" + actionBarHeight);
     }
 
-    @OnClick(R.id.button5)
     public void onButton5() {
         AlipayHelper.openAlipayScan(this);
     }
 
-    @OnClick(R.id.button6)
     public void onButton6() {
         AlipayHelper.openAlipayBarcode(this);
     }
 
-    @OnClick(R.id.button7)
     public void onButton7() {
         AlipayHelper.startAlipayClient(this, "aehvyvf4taua18zo6e");
     }
 
-    @OnClick(R.id.button8)
     public void onButton8() {
         ToastHelper.toast("程序是否为Debug:" + ApkHelper.isDebugMode(this));
     }
 
-    @OnClick(R.id.button9)
     public void onButton9() {
         ToastHelper.toast("meta:" + ApkHelper.getApplicationMetaValue(this, "CHANNEL"));
     }
 
 
-    @OnClick(R.id.button10)
     public void onButton10() {
         IntentHelper.launchApp(this, Constants.PACKAGE_WECHAT);
     }
 
-    @OnClick(R.id.button11)
     public void onButton11() {
         IntentHelper.gotoAppSettings(this);
     }
 
-    @OnClick(R.id.button12)
     public void onButton12() {
         if (MarketHelper.isMarketAvailable(this)) {
             MarketHelper.gotoMarket(this, Constants.PACKAGE_WECHAT);
@@ -109,22 +154,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @OnClick(R.id.button13)
     public void onButton13() {
         if (MarketHelper.isMarketAvailable(this)) {
-            MarketHelper.gotoGooglePlay(this,Constants.PACKAGE_WECHAT);
+            MarketHelper.gotoGooglePlay(this, Constants.PACKAGE_WECHAT);
         } else {
             ToastHelper.toast("请先安装应用市场!!!");
         }
     }
 
-    @OnClick(R.id.button14)
     public void onButton14() {
-        ToastHelper.toast("isPad:"+ScreenHelper.isPad(this));
+        ToastHelper.toast("isPad:" + ScreenHelper.isPad(this));
     }
 
-    @OnClick(R.id.button15)
     public void onButton15() {
-        ShortCutHelper.createShortCut(this,"001","jack",R.mipmap.ic_launcher,this.getClass());
+        ShortCutHelper.createShortCut(this, "001", "jack", R.mipmap.ic_launcher, this.getClass());
     }
 }
